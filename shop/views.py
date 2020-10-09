9b35d7384a18c616b518d0dc13fa3d0bcef2afa3
sami <samifarshori@gmail.com>
2020-10-09 20:27:06 +0530

    use javascript

--- shop/views.py
+++ shop/views.py
@@ -5,11 +5,24 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    products = product.objects.all()
-    print(products)
-    n = len(products)
-    nslides = n//4 + ceil((n/4)-(n//4))
-    params = {'no_of_slides':nslides,'range': range(1,nslides),'product':products}
+   # products = product.objects.all()
+   # print(products)
+    #n = len(products)
+   # nslides = n//4 + ceil((n/4)-(n//4))
+
+    allprods= []
+    catprods= product.objects.values('category','id')
+    cats = {item['category'] for item in catprods}
+    for cat in cats:
+        prod = product.objects.filter(category=cat)
+        n = len(prod)
+        nslides = n // 4 + ceil((n / 4) - (n // 4))
+        allprods.append([prod, range(1, nslides), nslides])
+
+   # params = {'no_of_slides':nslides,'range': range(1,nslides),'product':products}
+   #allprods = [[products, range(1, nslides), nslides],
+                #[products, range(1, nslides), nslides]]
+    params = {'allprods': allprods}
     return render(request,'shop/index.html',params)
 def about(request):
     return render(request, 'shop/about.html')
